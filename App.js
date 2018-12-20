5b4035a662b53c8fc234f8e0d7ce29ca89aaa5ae
myvary <727484328@qq.com>
2018-12-20 17:58:35 +0800

    添加路由，修改导航

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { StatusBar } from "react-native";
-import { createAppContainer,createStackNavigator } from "react-navigation";
-import Global from './src/libs/Global'
+import { StatusBar, View, Text } from "react-native";
+import { createAppContainer, createStackNavigator } from "react-navigation";
+import Global from "./src/libs/Global";
 import Login from "./src/pages/profile/login";
+import UserInfo from "./src/pages/profile/userInfo";
+import updateUserInfo from "./src/pages/profile/updateUserInfo";
 import TabNavigation from "./src/libs/TabNavigation";
-
+import FontAwesomeIcons from "react-native-vector-icons/FontAwesome";
 type Props = {};
 
 export default class App extends Component<Props> {
@@ -12,18 +14,46 @@ export default class App extends Component<Props> {
     return <AppContainer />;
   }
 }
-
-
+const otherNavigationOptions = {
+  headerTitleStyle: {
+    flex: 1,
+    alignSelf: "center",
+    fontSize: 18,
+    color: "#333",
+    textAlign: "center"
+  },
+  headerRight: <View />,
+  headerBackImage: (
+    <View
+      style={{
+        paddingRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        justifyContent: "center"
+      }}
+    >
+      <FontAwesomeIcons name="angle-left" size={25} color="#222" />
+    </View>
+  ),
+  headerStyle: {
+    elevation: 0
+  }
+};
 const HomeStack = createStackNavigator(
   {
     TabNavigation: { screen: TabNavigation },
-    Login: { screen: Login }
+    Login: { screen: Login, navigationOptions: { header: null } },
+    updateUserInfo: {
+      screen: updateUserInfo,
+      navigationOptions: otherNavigationOptions
+    },
+    UserInfo: { screen: UserInfo, navigationOptions: otherNavigationOptions }
   },
   {
     initialRouteName: "TabNavigation",
     initialRouteParams: { initPara: "初始页面参数" },
     headerMode: "float",
-    cardStyle: { backgroundColor: "#efefef" }
+    cardStyle: { backgroundColor: "#efefef", elevation: 0 }
   }
 );
 Global.globalStorage();
