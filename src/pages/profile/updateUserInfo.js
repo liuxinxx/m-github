b103791088c96b3a06ba0f13fdb9265fe2deea79
myvary <727484328@qq.com>
2018-12-21 17:17:53 +0800

    更新用户信息

--- src/pages/profile/updateUserInfo.js
+++ src/pages/profile/updateUserInfo.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { ScrollView, Text, View } from "react-native";
 import { List, InputItem, Toast, Icon } from "antd-mobile-rn";
-import { Button } from "react-native-paper";
+import { Button, Snackbar } from "react-native-paper";
 import userModel from "../..//model/userModel";
-import Global from "../../libs/Global"
+import FontAwesomeIcons from "react-native-vector-icons/FontAwesome";
+import Global from "../../libs/Global";
+const dismissKeyboard = require("dismissKeyboard");
 export default class updateUserInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       value: "",
       type: "",
-      disabled: true
+      disabled: true,
+      btn_loading: false,
+      visible: false,
+      title: ""
     };
   }
   static navigationOptions = ({ navigation }) => {
@@ -30,7 +35,8 @@ export default class updateUserInfo extends React.Component {
     this.updateNavigation();
     this.setState({
       value: this.props.navigation.state.params.value,
-      type: this.props.navigation.state.params.type
+      type: this.props.navigation.state.params.type,
+      title: this.props.navigation.state.params.title
     });
     globalStorage
       .load({
@@ -39,7 +45,7 @@ export default class updateUserInfo extends React.Component {
       .then(ret => {
         console.log("本地读取成功");
         console.log(ret);
-        Global.globalAccessToken(ret['access_token'])
+        Global.globalAccessToken(ret["access_token"]);
       })
       .catch(err => {});
   }
@@ -48,12 +54,34 @@ export default class updateUserInfo extends React.Component {
   }
   render() {
     return (
-      <ScrollView
-        style={{ flex: 1, backgroundColor: "#EFEFEF" }}
-        automaticallyAdjustContentInsets={false}
-        showsHorizontalScrollIndicator={false}
-        showsVerticalScrollIndicator={false}
-      >
+      <View style={{ flex: 1, backgroundColor: "#EFEFEF" }}>
+        <Snackbar
+          visible={this.state.visible}
+          duration={2000}
+          onDismiss={() => {
+            this.props.navigation.pop();
+          }}
+          style={{
+            backgroundColor: "#6abf47",
+            height: 35,
+            alignItems: "center",
+            margin: 10,
+            justifyContent: "center"
+          }}
+        >
+          <FontAwesomeIcons name={"check-circle"} size={19} />
+          <Text> </Text>
+          <Text
+            style={{
+              fontSize: 16,
+              padding: 10,
+              alignSelf: "center",
+              fontWeight: "700"
+            }}
+          >
+            更新「{this.state.title}」成功
+          </Text>
+        </Snackbar>
         <List>
           <View style={{ backgroundColor: "#EFEFEF", height: 10 }} />
           <InputItem
@@ -73,14 +101,23 @@ export default class updateUserInfo extends React.Component {
         <View style={{ margin: 10 }}>
           <Button
             disabled={this.state.disabled}
-            style={{ backgroundColor: "#36f" }}
             mode="contained"
             onPress={() => {
-              let body = { };
+              // 隐藏键盘
+              dismissKeyboard();
+              Toast.loading("正在更新", 60);
+              this.setState({
+                disabled: true
+              });
+              let body = {};
               body[this.state.type] = this.state.value;
               userModel
                 .updateUserInfo(body)
                 .then(data => {
+                  Toast.hide();
+                  this.setState({
+                    visible: true
+                  });
                   console.info(data);
                 })
                 .catch(error => {
@@ -91,7 +128,7 @@ export default class updateUserInfo extends React.Component {
             更新
           </Button>
         </View>
-      </ScrollView>
+      </View>
     );
   }
 }
